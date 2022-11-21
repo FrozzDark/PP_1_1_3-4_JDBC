2992e403d829c60f90d01d0c9b172bef505e1f82
FrozzDark <KiMaksim@proton.me>
2022-11-21 15:21:10 +0600

    Problem 1.1.4 solved commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Connection connection = null;
+
+    static {
+        String URL = "jdbc:mysql://localhost:3306/mydbtest";
+        String USERNAME = "root";
+        String PASSWORD = "root";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection() {
+        return connection;
+    }
 }
